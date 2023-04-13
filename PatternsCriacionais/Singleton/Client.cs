15191d64fd1e1f70a4ce94cb515d4d67c0e1346f
Jean Connory <michaelrhcp@gmail.com>
2023-04-12 23:02:28 -0300

    Singleton

---
+++ PatternsCriacionais/Singleton/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.PatternsCriacionais.Singleton
+{
+	public class Client
+	{
+		public void ConsumirDB()
+		{
+			var instancia = ContextDB.Instancia;
+			instancia.ExecutaQuery("SELECT * FROM Nome_Tabela;");
+
+			instancia = ContextDB.Instancia;
+			instancia.ExecutaQuery("INSERT INTO Nome_Tabela (valor) VALUES (0001);");
+
+			instancia = ContextDB.Instancia;
+			instancia.ExecutaQuery("UPDATE Nome_Tabela SET valor = 0002 WHERE Id = 1;");
+
+			instancia = ContextDB.Instancia;
+			instancia.ExecutaQuery("DELETE Nome_Tabela WHERE Id = 1;");
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 //using DesignPatterns.PatternsCriacionais.FactoryMethod;
-using DesignPatterns.PatternsCriacionais.AbstractFactory;
+
+//using DesignPatterns.PatternsCriacionais.AbstractFactory;
+
+using DesignPatterns.PatternsCriacionais.Singleton;
 
 Console.WriteLine("\n Design Patterns \n");
 
 Client client = new Client();
 //client.ExecutarCriacaoProduto();
-client.ConsultarRotinaAluno();
+//client.ConsultarRotinaAluno();
+client.ConsumirDB();
